ac7346ecfd14875dfa751898d39ba00f131a4219
*bKpsdIQhFF <r-otake@joyzo.co.jp>
2022-01-13 15:34:21 +0900

    UPDATE:モジュールの更新

--- src/js/customize.js
+++ src/js/customize.js
@@ -1,12 +1,12 @@
 (() => {
-  'use strict';
+  "use strict";
 
-  kintone.events.on('app.record.index.show', (event) => {
+  kintone.events.on("app.record.index.show", (event) => {
     console.log(event);
 
-    const div = document.createElement('div');
-    div.id = 'hello';
-    div.textContent = 'hello world';
+    const div = document.createElement("div");
+    div.id = "hello";
+    div.textContent = "hello world";
     kintone.app.getHeaderMenuSpaceElement().appendChild(div);
 
     return event;
